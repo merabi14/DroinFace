7b5311d840e7bf069bccca4a1d2220ef1efdffa4
Tamashis Shemqmneli <droinface@gmail.com>
2023-11-20 22:19:57 +0400

    First Configuration

---
+++ Assets/Scripts/PlayerMovment.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour
+{
+    public CharacterController2D characetrcontroller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixUpdate()
+    {
+    
+
+    }
+}
